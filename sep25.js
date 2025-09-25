6d8cc3d895801e10d2a589774434f3c267bde3a9
niamul007 <am3994566@gmail.com>
2025-09-25 11:13:33 +0600

    save second practice item

--- sep25.js
+++ sep25.js
@@ -1,3 +1,4 @@
+
 const todos = [
   { id: 1, text: "Learn JS", done: true },
   { id: 2, text: "Learn React", done: false },
@@ -18,10 +19,25 @@ const updated = makeDone.filter((item)=> item.done === true).map(data=>{
     return data.text;
 })
 
-console.log(updated)
+// console.log(updated)
 
 // ✅ Task 1: use .map() to mark todo with id 2 as done
 // ✅ Task 2: use .filter() to get only done todos
 // Expected: ["Learn JS", "Learn React"]
 
 
+const users = [
+  { name: "Alice", age: 22 },
+  { name: "Bob", age: 17 },
+  { name: "Charlie", age: 30 },
+  { name: "Diana", age: 15 }
+];
+
+const adults = users.filter((item)=> item.age > 18).map(data=>{
+    return data.name
+})
+console.log(adults)
+
+// ✅ Task 1: filter only users >= 18
+// ✅ Task 2: map their names into a new array
+// Expected: ["Alice", "Charlie"]
