9878250510212ac5d2b68cfab309412758af8c39
niamul007 <am3994566@gmail.com>
2025-09-30 21:52:48 +0600

    add all

---
+++ funtion.js
@@ -0,0 +1,8 @@
+function createPersonalizedGreeting(name, timeOfDay) {
+    // Directly return the template literal
+    return `Good ${timeOfDay}, ${name}! We hope you have a great time.`; 
+}
+createPersonalizedGreeting("Sarah","Night")
+
+
+
