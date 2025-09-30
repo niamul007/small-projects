bd6600fb88a98f02c6541112e552c31c09ac1c4a
niamul007 <am3994566@gmail.com>
2025-09-30 12:56:48 +0600

    add while

---
+++ condition.js
@@ -0,0 +1,18 @@
+let n = 9;
+
+if(n%2===0){
+    console.log(`number is even : ${n}`)
+}else{
+    console.log(`number is odd ${n}`)
+}
+
+let sum = 0;
+for(let i = 0; i<10; i++){
+    console.log(i*3)
+}
+console.log(sum)
+let i =0 ;
+while (i <20) {
+    console.log("fuck")
+    i++;
+}
