f8c3ead9842c0380bc2fdddfcecb523b2bd34703
niamul007 <am3994566@gmail.com>
2025-10-01 15:48:49 +0600

    add jonior code

--- funtion.js
+++ funtion.js
@@ -4,5 +4,33 @@ function createPersonalizedGreeting(name, timeOfDay) {
 }
 createPersonalizedGreeting("Sarah","Night")
 
+const celsiusToFahrenheit = (celcius)=> (celcius*9/5) + 32;
+console.log(celsiusToFahrenheit(50))
 
 
+
+const numbers = [10, 20, 5, 15];
+let sum =0;
+function sumArry(arr){
+    for(let i =0; i< arr.length; i++){
+        sum+= arr[i];
+    }
+    return sum;
+}
+
+console.log(sumArry(numbers));
+
+
+
+function checkTicketEligibility(isStudent,stdId){
+    if(isStudent=== true && stdId === true){
+        console.log("Discounted Ticket")
+    }else if(isStudent=== true && stdId === false){
+        console.log("Full Price Ticket");
+    }else{
+        console.log("Standard Price");
+    }
+    return;
+}
+
+console.log(checkTicketEligibility(true,false))
