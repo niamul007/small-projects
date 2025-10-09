4de038e2351b7110691e06bc9eaa963e6447765d
niamul007 <am3994566@gmail.com>
2025-10-09 14:05:45 +0600

    adding optional chaining

--- oct9.js
+++ oct9.js
@@ -1,11 +1,19 @@
 const arr = ["niamul","rahul","nishita","tommy"];
-
 function firstAndLast(arry){
-    let newArr = [...arry];
-    return [newArr.pop() , newArr.shift()];
+    let first = arry[0];
+    let last = arry[arry.length - 1];
+    return [first,last]
 }
-
 console.log(firstAndLast(arr));
+console.log(arr)
+
+//Try better way around
+// function firstAndLast(arry){
+//     let newArr = [...arry];
+//     return [newArr.pop() , newArr.shift()];
+// }
+
+// console.log(firstAndLast(arr));
 
 const person = {
     name: "Alex",
@@ -16,3 +24,20 @@ const newFun = ({name,job})=>{
     return `${name} is a ${job}`;
 } 
 console.log(newFun(person));
+
+
+
+const config = { 
+    style: { 
+        header: { 
+            fontSize: 16 
+        }
+    }
+};
+const size = config.style.header.fontSize;
+console.log(size);
+
+// Write your single line of code here using optional chaining to get fontSize from `data`:
+// const size = ... ;
+
+// console.log(size);
